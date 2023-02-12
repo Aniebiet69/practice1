3b3bf1a9bc6db3795c94b666e95f683ef3ddba01
Aniebiet Archibong <102147581+Surebo-i@users.noreply.github.com>
2023-02-12 15:23:20 +0900

    changes

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -18,17 +18,21 @@ const Menu = () =>(
 
 const Navbar = () => {
 
-  const {toggleMenu, setToggleMenu} = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className='gpt3__navbar'>
+
         <div className='gpt3__navbar-links'>
-          <div className='gpt3__navbar-links_logo'>
+
+        <div className='gpt3__navbar-links_logo'>
             <img src= {logo} alt="logo" />
           </div>
 
           <div className='gpt3__navbar-links_container'>
             <Menu />
           </div>
+
+        </div>
       
           <div className='gpt3__navbar-sign'>
             <p>Sign in</p>
@@ -46,12 +50,21 @@ const Navbar = () => {
                 <div>
                   <Menu />
                 </div>
+
+
+                
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                  <p>Sign in</p>
+                  <button>Sign up</button>
+                </div>
+
+
               </div>
             )
 
             }
           </div>
-        </div>
+      
       
       </div>
   )
